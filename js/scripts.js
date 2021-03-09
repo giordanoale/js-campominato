8e4e88414553d3650521141c7f78c8c9e07a7aba
giordanoale <a.giordano89@gmail.com>
2021-03-09 16:21:11 +0100

    Il programma chiede all'utente di inserire dei numeri diversi compresi da 1 a 100 e li aggiunge nell'array

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,9 @@ var bomba;
 var i = 0;
 
 while (i < 16) {
+
   bomba = Math.floor(Math.random() * 100) + 1;
+
   if (!(bombe.includes(bomba))){
     bombe.push(bomba);
     i++
@@ -20,8 +22,23 @@ console.log(bombe.length);
 // In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
 // L’utente non può inserire più volte lo stesso numero.
 
+var numeri = [];
+var utente;
 
+var j = 0;
 
+while (j < 10) {
+
+  utente = parseInt(prompt("Inserisci un numero da 1 a 100"));
+
+  if (!(numeri.includes(utente)) && !(isNaN(utente)) && !(utente < 1 || utente > 100)) {
+    numeri.push(utente);
+    j++
+  }
+  console.log(numeri);
+}
+
+console.log(numeri.length);
 // Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
 // La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
 // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
